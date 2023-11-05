d9c06fa82ecd2115cfc92c0d42bda3073996008b
Ryan Chiang <wsad71155@gmail.com>
2023-11-05 19:02:57 +0800

    收藏按鈕動態

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
+const collectBtn = document.querySelectorAll(".collect-btn");
+console.log(collectBtn[0]);
+collectBtn.forEach(function (item) {
+
+    item.addEventListener("click", function (e) {
+        if (e.target.value == "collected") {
+            // collectBtn.innerHTML =`<i class="fa-solid fa-heart"></i>`
+            item.value = "uncollect";
+            item.classList.remove("fa-regular");
+            item.classList.add("fa-solid");
+
+        } else if (e.target.value == "uncollect") {
+            // collectBtn.innerHTML =`<i class="fa-regular fa-heart"></i>`
+            item.value = "collected";
+            item.classList.add("fa-regular");
+            item.classList.remove("fa-solid");
+        }
+    });
+})
