4d4c1a4383ae4fa6074d413ad6f701800099e187
Ryan Chiang <wsad71155@gmail.com>
2023-11-11 01:12:14 +0800

    Updates

--- assets/index-8e958f87.js
+++
@@ -1,1 +0,0 @@
-import"./main-5d2a492c.js";const e=document.querySelector("#hi");e.addEventListener("click",function(t){alert("hello")});
---
+++ assets/index-e86f60bd.js
@@ -0,0 +1,1 @@
+import"./main-8b0c641a.js";const e=document.querySelector("#hi");e.addEventListener("click",function(t){alert("hello")});
