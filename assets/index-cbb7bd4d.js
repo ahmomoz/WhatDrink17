e4864264e84d2f994600fe91134760d9d67361c2
shiro <a9944sam@gmail.com>
2023-11-11 21:51:05 +0800

    Updates

---
+++ assets/index-cbb7bd4d.js
@@ -0,0 +1,1 @@
+import"./main-939ec6d3.js";const e=document.querySelector("#hi");e.addEventListener("click",function(t){alert("hello")});
--- assets/index-e86f60bd.js
+++
@@ -1,1 +0,0 @@
-import"./main-8b0c641a.js";const e=document.querySelector("#hi");e.addEventListener("click",function(t){alert("hello")});
