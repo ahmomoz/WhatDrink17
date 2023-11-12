c3355221bf5606d581d2a75ee99de7460ed70c74
MY <x5426854268@gmail.com>
2023-11-12 21:36:07 +0800

    JS檔案分頁

---
+++ assets/js/drinks.js
@@ -0,0 +1,56 @@
+//飲料資訊頁----------------------------------------------------------------------
+
+//變數命名
+const drinkList = document.querySelector("#drinkList");
+let drinkData = [];
+let drinkTag;
+
+function drinkTagPush(){
+  drinkTag = drinkData.map(function(item,index){
+    return `${item.TeaType},${item.Ingredients}`
+  });
+  console.log(drinkTag);
+};
+
+//載入預設飲料卡片------------------------------------------------
+const drinkRender = () => {
+    let str='';
+    drinkData.forEach(function(item){
+        str+=`
+        <li id="drinkListItem" class="drinks-card px-16 py-24 px-md-24">
+        <button type="button" class="collect-btn border-0 text-primary fa-regular fa-heart fs-24" value="collected"></button>
+        <img src="${item.ImageLink}">
+        <div class="drinks-card-body ms-16">
+          <h4 class="mb-8 mb-md-12">${item.DrinkName}</h4>
+          <ul class="drinks-tag-group mb-8 mb-md-12">
+            <li class="drinks-tag">${drinkTag}</li>
+          </ul>
+          <p class="drinks-card-content mb-24 mb-md-32">${item.Description}</p>
+          <a href="#" class="d-block text-primary text-end"><span
+              class="material-symbols-outlined me-2s align-middle">
+              location_on
+            </span>搜尋店家</a>
+        </div>
+      </li>
+        `;
+    });
+    drinkList.innerHTML = str;
+};
+
+//初始化預設飲料卡片函式--------------------------------------------
+function drinkRenderData(){
+  drinkRender();
+};
+
+
+//將飲料資料由外部寫入
+axios.get('https://json-server-project-wtkt.onrender.com/drinks')
+.then(function(response){
+    drinkData = response.data;
+    drinkRenderData();
+    drinkTagPush();
+})
+.catch(error => {
+  console.error('Error fetching data:', error);
+});
+
