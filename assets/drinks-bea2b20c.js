ab3210bf8f1f0ab7d1099726b0ac0b46e07893d4
MY <x5426854268@gmail.com>
2023-11-13 21:03:23 +0800

    Updates

--- assets/drinks-bea2b20c.js
+++ assets/drinks-bea2b20c.js
@@ -1,11 +1,11 @@
-import"./main-751fc6a2.js";const a=document.querySelector("#drinkList");let e=[],s=[];function i(){const n=e.map(function(t,c){return t.Ingredients.length===0?`${t.TeaType}`:`${t.TeaType},${t.Ingredients}`}),r=[];for(let t=0;t<n.length;t++)r.push(n[t].split(","));console.log(r),s=r}const d=()=>{let n="";e.forEach(function(r){n+=`
+import"./main-751fc6a2.js";const o=document.querySelector("#drinkList");let s=[],d=[];function l(){const t=s.map(function(n,i){return n.Ingredients.length===0?`${n.TeaType}`:`${n.TeaType},${n.Ingredients}`}),r=[];for(let n=0;n<t.length;n++)r.push(t[n].split(","));let e="",a=[];r.forEach(function(n){n.forEach(function(i,c){e+=`<li class="drinks-tag">${i}</li>`,c===n.length-1&&(a.push(e),e="")}),d.push(a),a=[]})}const u=()=>{let t="";s.forEach(function(r){t+=`
         <li id="drinkListItem" class="drinks-card px-16 py-24 px-md-24">
         <button type="button" class="collect-btn border-0 text-primary fa-regular fa-heart fs-24" value="collected"></button>
         <img src="${r.ImageLink}">
         <div class="drinks-card-body ms-16">
           <h4 class="mb-8 mb-md-12">${r.DrinkName}</h4>
           <ul class="drinks-tag-group mb-8 mb-md-12">
-            <li class="drinks-tag">${s[r.id-1]}</li>
+            ${d[r.id-1]}
           </ul>
           <p class="drinks-card-content mb-24 mb-md-32">${r.Description}</p>
           <a href="#" class="d-block text-primary text-end"><span
@@ -14,4 +14,4 @@ import"./main-751fc6a2.js";const a=document.querySelector("#drinkList");let e=[]
             </span>搜尋店家</a>
         </div>
       </li>
-        `}),a.innerHTML=n};function o(){d()}axios.get("https://json-server-project-wtkt.onrender.com/drinks").then(function(n){e=n.data,i(),o()}).catch(n=>{console.error("Error fetching data:",n)});
+        `}),o.innerHTML=t};function p(){u()}axios.get("https://json-server-project-wtkt.onrender.com/drinks").then(function(t){s=t.data,l(),p()}).catch(t=>{console.error("Error fetching data:",t)});
