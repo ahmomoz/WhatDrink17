3c0cb8ff2211f5c21239558b2c70f25c5924980b
MY <x5426854268@gmail.com>
2023-11-13 21:45:24 +0800

    Updates

--- assets/stores-list-2aedddc5.js
+++ assets/stores-list-2aedddc5.js
@@ -1,4 +1,4 @@
-import"./main-751fc6a2.js";const r=document.querySelector("#storeList");let a=[];function e(){l()}axios.get("https://json-server-project-wtkt.onrender.com/shops").then(function(s){a=s.data,e()}).catch(s=>{console.error("Error fetching data:",s)});const l=()=>{let s="";a.forEach(function(t){s+=`
+import"./main-751fc6a2.js";const l=document.querySelector("#storeList");let r=[],o=[];const i=()=>{r.forEach(function(s){const t={uber:"uber eat",foodpanda:"food panda",hasEvent:"合作活動"};let a="";for(const e in t)s[e]&&(a+=`<li class="stores-tag">${t[e]}</li>`);o.push(a),a=""})},c=()=>{let s="";r.forEach(function(t){s+=`
         <li id="storeListItem" class="stores-card px-16 py-24 mb-40 px-md-24">
             <button type="button" class="collect-btn border-0 text-primary fa-regular fa-heart fs-24" value="collected"></button>
             <img src="${t.logo}">
@@ -16,11 +16,9 @@ import"./main-751fc6a2.js";const r=document.querySelector("#storeList");let a=[]
               <p class="stores-card-content mb-16">
                 ${t.Description}</p>
               <ul class="stores-tag-group mb-16">
-                <li class="stores-tag">uber eat</li>
-                <li class="stores-tag">food panda</li>
-                <li class="stores-tag">合作活動</li>
+                ${o[t.id-1]}
               </ul>
               <a href="stores-info.html" class="stores-card-btn">查看店家資訊</a>
             </div>
           </li>
-        `}),r.innerHTML=s};
+        `}),l.innerHTML=s},n=()=>{c()};axios.get("https://json-server-project-wtkt.onrender.com/shops").then(s=>{r=s.data,i(),n()}).catch(s=>{console.error("Error fetching data:",s)});
