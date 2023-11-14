f999f274abddf05db760418c715d7fac5deba3ec
Ryan Chiang <wsad71155@gmail.com>
2023-11-14 23:35:19 +0800

    驗證收藏功能

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,16 +1,17 @@
-const collectBtn = document.querySelectorAll(".collect-btn");
-console.log(collectBtn);
+const collectBtn = document.querySelectorAll(".collect-btn"); //抓按鈕class
+console.log(collectBtn);//驗證
 function isCollect(){
+    //跑 querySelectorAll 陣列
     collectBtn.forEach(function (item) {
+        //監聽按鈕
         item.addEventListener("click", function (e) {
+            //還沒收藏時，value 預設傳送 collected，點擊後改傳uncollect，並移除外框樣式class、新增填滿樣式class
             if (e.target.value == "collected") {
-                // collectBtn.innerHTML =`<i class="fa-solid fa-heart"></i>`
                 item.value = "uncollect";
                 item.classList.remove("fa-regular");
                 item.classList.add("fa-solid");
-    
+            //已經收藏時，value 已改成傳送 uncollect，點擊後變為 collected，並移除填滿樣式class，新增外框樣式class
             } else if (e.target.value == "uncollect") {
-                // collectBtn.innerHTML =`<i class="fa-regular fa-heart"></i>`
                 item.value = "collected";
                 item.classList.add("fa-regular");
                 item.classList.remove("fa-solid");
