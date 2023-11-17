b96d31d6085e87ac9a17a0abc59fcbfd0354c6b5
Ryan Chiang <wsad71155@gmail.com>
2023-11-18 01:10:36 +0800

    Updates

--- assets/news-list-9e79f501.js
+++
@@ -1,1 +0,0 @@
-import"./bootstrap.min-cef82647.js";document.querySelector("#newsCard");console.log("test");axios.get("https://json-server-project-wtkt.onrender.com/latestNews").then(e=>{console.log(e.data)});
