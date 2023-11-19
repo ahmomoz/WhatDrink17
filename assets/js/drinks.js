d255a1448388571c00a1650455018526e9740901
MY <x5426854268@gmail.com>
2023-11-19 21:47:11 +0800

    修改頁碼樣式

--- assets/js/drinks.js
+++ assets/js/drinks.js
@@ -143,21 +143,31 @@ function displayFilteredData(data) {  //用於更新畫面的函數
 
 //分頁邏輯------------------------------------------------------
 
-const renderPagination = (pageData) =>{
 
-const totalItems = pageData.length; //飲料卡片數量
+/**const renderPagination = async () => {
+
 const itemsPerPage = 10;  //每頁顯示十個卡片
-const totalPages = Math.ceil(totalItems / itemsPerPage); //計算總頁數
 let currentPage = 1;  //當前頁數，預設為第一頁
 
-function displayData(page) {   //計算目前頁面顯示的卡片數量範圍
-  const startIndex = (page - 1) * itemsPerPage;
-  const endIndex = startIndex + itemsPerPage;
-  // 獲取目前頁面的數據
-  const currentPageData = pageData.slice(startIndex, endIndex);
+async function fetchData(page) {
+  try {
+    const response = await axios.get(`https://json-server-project-wtkt.onrender.com/drinks`, {
+      params: {
+        _page: page,
+        _limit: itemsPerPage,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
+
+async function displayData(page) {   
+  const pageData = await fetchData(page);
 
   // 渲染當前頁面的數據
-  renderCards(currentPageData);
+  renderCards(pageData);
 
   // 渲染分頁按鈕
   renderPaginationButtons();
@@ -278,9 +288,165 @@ function onPageButtonClick(page) {
 
 // 初始化頁面顯示
 displayData(currentPage);
+};**/
+
+const renderPagination = (pageData) =>{
+
+const totalItems = pageData.length; //飲料卡片數量
+const itemsPerPage = 10;  //每頁顯示十個卡片
+const totalPages = Math.ceil(totalItems / itemsPerPage); //計算總頁數
+let currentPage = 1;  //當前頁數，預設為第一頁
+
+function displayData(page) {   //計算目前頁面顯示的卡片數量範圍
+  const startIndex = (page - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  // 獲取目前頁面的數據
+  const currentPageData = pageData.slice(startIndex, endIndex);
+
+  // 渲染當前頁面的數據
+  renderCards(currentPageData);
+
+  // 渲染分頁按鈕
+  renderPaginationButtons();
 };
 
+function renderCards(data) {
+  drinkList.innerHTML = ""; // 清空容器
 
+  let str='';
+  data.forEach(item => {
+    str+=`
+          <li class="drinks-card px-16 py-24 px-md-24">
+            <button type="button" class="collect-btn border-0 text-primary fa-regular fa-heart fs-24"
+              value="collected"></button>
+            <img src="${item.ImageLink}" alt="drink image">
+            <div class="w-100 d-flex flex-column justify-content-between">
+              <div class="drinks-card-body ms-16">
+                <h4 class="mb-8 mb-md-12">${item.DrinkName}</h4>
+                <ul class="drinks-tag-group mb-8 mb-md-12">
+                  ${drinkTagAry[item.id-1]}
+                </ul>
+                <p class="drinks-card-content mb-24 mb-md-32">${item.Description}</p>
+              </div>
+              <a href="#" class="d-block text-primary text-end"><span
+                  class="material-symbols-outlined me-2s align-middle">
+                  location_on
+                </span>搜尋店家</a>
+            </div>
+          </li>
+        `;
+    });
+    drinkList.innerHTML = str;
+};
+
+//渲染分頁按鈕的邏輯，根據前頁和總頁數產生分頁按鈕
+function renderPaginationButtons() {
+  const buttonsContainer = document.getElementById("pagination");
+  buttonsContainer.innerHTML = ""; // 清空按鈕容器
+
+  // 生成頁碼按鈕
+  let str='';
+  for (let i = 1; i <= totalPages; i++) {
+    const isActive = i === currentPage ? 'active' : ''; // 檢查是否為當前頁面，是的話加上active樣式
+    str += `
+      <li class="page-item mx-4 ${isActive}">   
+        <a class="page-link" href="#" data-page="${i}">${i}</a>
+      </li>
+    `;
+  };
+  const preIsGray = currentPage === 1 ? 'text-gray' : ''; //檢查當前頁面是否為1，是的話加上灰色樣式
+  const nextIsGray = currentPage === totalPages ? 'text-gray' : ''; //檢查當前頁面是否為最後一頁，是的話加上灰色樣式
+
+  buttonsContainer.innerHTML =`
+    <li id="first-page-btn" class="page-item mx-4 d-none d-md-block">
+      <a class="page-link ${preIsGray}" href="#" aria-label="Previous">
+        <span class="material-symbols-outlined align-middle">keyboard_double_arrow_left</span>
+      </a>
+    </li>
+
+    <li id="previous-page-btn" class="page-item mx-4 d-none d-md-block">
+      <a class="page-link ${preIsGray}" href="#" aria-label="Previous">
+        <span class="material-symbols-outlined align-middle">chevron_left</span>
+      </a>
+    </li>
+
+    ${str}
+
+    <li id="next-page-btn" class="page-item mx-4 d-none d-md-block">
+      <a class="page-link ${nextIsGray}" href="#" aria-label="Next">
+        <span class="material-symbols-outlined align-middle">chevron_right</span>
+      </a>
+    </li>
+
+    <li id="last-page-btn" class="page-item mx-4 d-none d-md-block">
+      <a class="page-link ${nextIsGray}" href="#" aria-label="Next">
+        <span class="material-symbols-outlined align-middle">keyboard_double_arrow_right</span>
+      </a>
+    </li>`;
+
+    // 設定切頁按鈕事件監聽
+    const pageLinks = document.querySelectorAll(".page-link");
+    pageLinks.forEach(link => {
+      link.addEventListener("click", (e) => {
+        e.preventDefault(); //取消a連結效果
+        const pageNumber = parseInt(link.dataset.page);
+        onPageButtonClick(pageNumber);
+      });
+    })
+
+    // 前一頁按鈕事件監聽器
+    const previousPageBtn = document.getElementById("previous-page-btn");
+    previousPageBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      if (currentPage > 1) {
+        onPageButtonClick(currentPage - 1);
+      };
+    });
+
+    // 後一頁按鈕事件監聽器
+    const nextPageBtn = document.getElementById("next-page-btn");
+    nextPageBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      onPageButtonClick(currentPage + 1);
+    });
+
+    // 最前頁按鈕事件監聽器
+    const firstPageBtn = document.getElementById("first-page-btn");
+    firstPageBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      onPageButtonClick(1);
+    });
+
+    // 最後頁按鈕事件監聽器
+    const lastPageBtn = document.getElementById("last-page-btn");
+    lastPageBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      onPageButtonClick(totalPages);
+    });
+};
+function updateButtons() {
+  previousPageBtn.disabled = currentPage === 1;
+  nextPageBtn.disabled = currentPage === totalPages;
+  firstPageBtn.disabled = currentPage === 1;
+  lastPageBtn.disabled = currentPage === totalPages;
+  // 其他按鈕的更新邏輯（例如 next、first、last 等）
+};
+
+function onPageButtonClick(page) {
+  // 頁面按鈕點擊事件
+  currentPage = page;
+  displayData(currentPage);
+  updateButtons();
+  window.scrollTo({      //讓頁面跑到最上方的語法
+    top: 0,
+    behavior: 'smooth', // 使用平滑的滾動效果
+  });
+};
+// 初始化頁面顯示
+displayData(currentPage);
+updateButtons();
+
+};
 
 //將飲料資料由外部寫入
 axios.get('https://json-server-project-wtkt.onrender.com/drinks')
