38ba48872ad6f0e12c936e29c677310b31503354
shiro <a9944sam@gmail.com>
2023-11-19 14:49:37 +0800

    Updates

--- assets/news-list-42d60e4a.js
+++
@@ -1,1 +0,0 @@
-import"./bootstrap.min-dcd01d0b.js";document.querySelector("#newsCard");console.log("test");axios.get("https://json-server-project-wtkt.onrender.com/latestNews").then(e=>{console.log(e.data)});
---
+++ assets/news-list-83b05cd5.js
@@ -0,0 +1,1 @@
+import"./bootstrap.min-0eea227d.js";axios.get("https://json-server-project-wtkt.onrender.com/latestNews").then(t=>{console.log(t.data)});
