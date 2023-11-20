9c9a32310c4ffec6f3326036c80ce7c3b9fa180a
shiro <a9944sam@gmail.com>
2023-11-20 23:01:15 +0800

    Updates

--- assets/news-list-7a9fb27c.js
+++ assets/news-list-7a9fb27c.js
@@ -1,4 +1,4 @@
-import"./bootstrap.min-a7e784e5.js";const c=document.querySelector(".newsArea");function l(e){let t="";for(let s=0;s<e.length;s++){const i=e[s].Description.substring(0,100);t+=`<div class="col-12 news-card mb-0" id="newsCard">
+import"./bootstrap.min-a7e784e5.js";const l=document.querySelector(".newsArea");function c(e){let t="";for(let s=0;s<e.length;s++){const i=e[s].Description.substring(0,100);t+=`<div class="col-12 news-card mb-0" id="newsCard">
         <div class="card border-0">
             <div class="row">
                 <div class="col-12 col-lg-6 px-12">
@@ -11,11 +11,11 @@ import"./bootstrap.min-a7e784e5.js";const c=document.querySelector(".newsArea");
                         <p class="card-text">
                         ${i}...</p>
                         <div class="card-footer d-flex justify-content-end align-items-end">
-                            <p class="mb-0 text-right align-items-end"><a href="#">繼續閱讀 ></a></p>
+                            <p class="mb-0 text-right align-items-end"><a href="news-info.html?id=${e[s].id}">繼續閱讀 ></a></p>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <hr class="mb-40 mt-20 mt-lg-40">`}console.log(t),c.innerHTML=t}axios.get("https://json-server-project-wtkt.onrender.com/latestNews").then(e=>{let t=e.data;l(t)});
+    <hr class="mb-40 mt-20 mt-lg-40">`}console.log(t),l.innerHTML=t}axios.get("https://json-server-project-wtkt.onrender.com/latestNews").then(e=>{let t=e.data;c(t)});
