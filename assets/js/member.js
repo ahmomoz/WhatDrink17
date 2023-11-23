a33e8292810bba752033a8de5cb084fa866cea5d
shiro <a9944sam@gmail.com>
2023-11-23 23:17:25 +0800

    會員頁店家收藏列表部分完成

--- assets/js/member.js
+++ assets/js/member.js
@@ -1,8 +1,11 @@
 //頂部使用者資料
 const userDataArea = document.querySelector(".userData");
 
-//下方飲料列表
+//下方飲料收藏列表
 const drinkCollectionsArea = document.querySelector(".drinkCollections");
+
+//下方的店家收藏列表
+const shopCollectionsArea = document.querySelector(".shopCollectionsArea");
 
 // 渲染使用者資料
 // 目前還沒結合登入，暫時寫死userId=1的地方
@@ -39,6 +42,39 @@ function renderUserDrinkCollections(data) {
   drinkCollectionsArea.innerHTML = text;
 }
 
+// 渲染收藏店家列表資料
+// 店家標籤&查看店家資訊待補
+function renderUserShopCollections(data) {
+  let text = "";
+  for (let i = 0; i < data.length; i++) {
+    text += `<li class="stores-card px-16 py-24 px-md-24">
+    <button type="button" class="collect-btn border-0 text-primary fa-solid fa-heart fs-24" value="uncollect"></button>
+    <img src="${data[i].logo}" class="mb-8" alt="store image">
+    <div class="stores-card-body">
+      <div class="d-flex justify-content-between">
+        <h5 class="mb-8 mb-md-12">${data[i].name}</h5>
+        <ul class="d-flex text-primary">
+          <li><i class="fa-solid fa-star"></i></li>
+          <li><i class="fa-solid fa-star"></i></li>
+          <li><i class="fa-solid fa-star"></i></li>
+          <li><i class="fa-solid fa-star"></i></li>
+          <li><i class="fa-regular fa-star"></i></li>
+        </ul>
+      </div>
+      <p class="stores-card-content mb-16">
+      ${data[i].Description}</p>
+      <ul class="stores-tag-group mb-16">
+        <li class="stores-tag">uber eat</li>
+        <li class="stores-tag">food panda</li>
+        <li class="stores-tag">合作活動</li>
+      </ul>
+      <a href="stores-info.html" class="stores-card-btn">查看店家資訊</a>
+    </div>
+  </li>`
+  }
+  shopCollectionsArea.innerHTML = text;
+}
+
 //自server取得使用者資料後執行renderUserData
 axios.get('https://json-server-project-wtkt.onrender.com/users?id=1')
   .then(response => {
@@ -46,8 +82,7 @@ axios.get('https://json-server-project-wtkt.onrender.com/users?id=1')
     renderUserData(userData)
   });
 
-//自server取得該使用者收藏清單的drinkId列表
-//並轉化成飲料資料的矩陣
+//自server取得該使用者收藏清單的drinkId列表並轉化成飲料資料的矩陣
 axios.get(`https://json-server-project-wtkt.onrender.com/userDrinkCollections?userId=1`)
   .then(response => {
     const userDrinkCollections = response.data;
@@ -61,6 +96,26 @@ axios.get(`https://json-server-project-wtkt.onrender.com/userDrinkCollections?us
       .then(response => {
         console.log(response.data);
         renderUserDrinkCollections(response.data);
+      })
+  })
+  .catch(error => {
+    console.error('發生錯誤:', error);
+  });
+
+//自server取得該使用者收藏清單的storeId列表並轉化成飲料資料的矩陣
+axios.get(`https://json-server-project-wtkt.onrender.com/userShopCollections?userId=1`)
+  .then(response => {
+    const usershopCollections = response.data;
+    // 抓取全部shopId
+    const shopIds = usershopCollections.map(item => item.shopId);
+    // 組成取得店家資料的URL
+    const shopsUrl = `https://json-server-project-wtkt.onrender.com/shops?id=${shopIds.join('&id=')}`;
+
+    // 取得店家資料
+    axios.get(shopsUrl)
+      .then(response => {
+        console.log(response.data);
+        renderUserShopCollections(response.data);
       })
   })
   .catch(error => {
