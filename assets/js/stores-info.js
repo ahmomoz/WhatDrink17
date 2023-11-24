be07bc543683a790e2aafda510f5f6d0d20c7999
Ryan Chiang <wsad71155@gmail.com>
2023-11-24 22:04:48 +0800

    店家資訊麵包屑完成

--- assets/js/stores-info.js
+++ assets/js/stores-info.js
@@ -2,48 +2,63 @@
 const storeInfo = document.querySelector("#storeInfo"); //卡片列表
 let storeInfoData = [];
 let storeTagAry = [];
+const breadcrumb = document.querySelector(".breadcrumb");
+
 import axios from 'axios';
 const storeId = window.location.href.split("?id=").pop()
 
 //將店家資料由外部寫入
 axios.get(`https://json-server-project-wtkt.onrender.com/shops?id=${storeId}`)
-    .then(response => {
-        storeInfoData = response.data;
-        storeInfoRenderData(); //載入預設飲料卡片
-        storeTagPush();
-        // console.log(storeInfoData);
-    })
-    .catch(error => {
-        console.error('Error fetching data:', error);
-    });
+  .then(response => {
+    storeInfoData = response.data;
+    breadcrumbPush(); //載入麵包屑路徑
+    storeTagPush(); //載入店家標籤
+    storeInfoRenderData(); //載入個別店家卡片
+    // console.log(storeInfoData);
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
 
+const breadcrumbPush = () => {
+  let str = ''
+  storeInfoData.forEach(item => {
+    str += `
+    <li class="breadcrumb-item d-inline"><a href="index.html" class="breadcrumb-item fs-16">首頁</a></li>
+    <li class="breadcrumb-item d-inline"><a href="stores-list.html" class="breadcrumb-item fs-16">熱門店家</a></li>
+    <li class="breadcrumb-item active d-inline" aria-current="page" class="fs-16">${item.name}</li>
+  `;
+  })
+
+  breadcrumb.innerHTML = str;
+}
 //店家tag組合函式----------------------------------------
-const storeTagPush = () =>{      //合併外送、合作活動成一個陣列
-    const storeTag = storeInfoData.forEach(item => {
-      const tags = {
-        uber: 'uber eat',
-        foodpanda: 'food panda',
-        hasEvent: '合作活動'
-      };
-      let tagStr = '';
-      for (const key in tags) {
-        if (item[key]) {
-          tagStr += `<li class="stores-tag">${tags[key]}</li>`;
-        }
+const storeTagPush = () => {      //合併外送、合作活動成一個陣列
+  const storeTag = storeInfoData.forEach(item => {
+    const tags = {
+      uber: 'uber eat',
+      foodpanda: 'food panda',
+      hasEvent: '合作活動'
+    };
+    let tagStr = '';
+    for (const key in tags) {
+      if (item[key]) {
+        tagStr += `<li class="stores-tag">${tags[key]}</li>`;
       }
-      storeTagAry.push(tagStr);
-      tagStr=''; // 清空tagStr，避免重複推入
-    });
-    // console.log(storeTagAry[0]);
+    }
+    storeTagAry.push(tagStr);
+    tagStr = ''; // 清空tagStr，避免重複推入
+  });
+  // console.log(storeTagAry[0]);
 
-  };
+};
 
 //載入預設分店卡片函式------------------------------------------------
 const storeInfoRender = () => {
-    storeTagPush();
-    let str = '';
-    storeInfoData.forEach(item => {
-        str += `              
+  storeTagPush();
+  let str = '';
+  storeInfoData.forEach(item => {
+    str += `              
         <div class="row">
         <div class="col-12 col-md-4">
           <img src="${item.logo}" class="w-100" alt="logo">
@@ -85,14 +100,14 @@ const storeInfoRender = () => {
         </div>
       </div>
 `;
-    });
+  });
 
-    storeInfo.innerHTML = str;
-    // console.log(storeTagAry[0]);
+  storeInfo.innerHTML = str;
+  // console.log(storeTagAry[0]);
 
 };
 
 //初始化預設飲料卡片函式--------------------------------------------
 const storeInfoRenderData = () => {
-    storeInfoRender();
+  storeInfoRender();
 };
