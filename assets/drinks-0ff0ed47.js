ca84590005f03fb8e72ed1328f6abb4e5bb48431
MY <x5426854268@gmail.com>
2023-12-01 22:21:30 +0800

    Updates

--- assets/drinks-0ff0ed47.js
+++ assets/drinks-0ff0ed47.js
@@ -1,4 +1,4 @@
-import"./bootstrap.min-66c8f441.js";const p=document.querySelector("#drinkList");let v=document.querySelector("#teaTypeSelect"),y=document.querySelector("#ingredientsSelect"),L=[],u=[],k=[];const b=sessionStorage.getItem("user_id"),C=()=>{const a=u.map(t=>!t||!t.TeaType?"":t.Ingredients.length===0?`${t.TeaType}`:`${t.TeaType},${t.Ingredients}`),e=[];for(let t=0;t<a.length;t++)e.push(a[t].split(","));let n="",s=[];e.forEach(t=>{t.forEach((m,o)=>{n+=`<li class="drinks-tag">${m}</li>`,o===t.length-1&&(s.push(n),n="")}),k.push(s),s=[]})},_=()=>{let a="";u.forEach(e=>{a+=`
+import"./bootstrap.min-66c8f441.js";const u=document.querySelector("#drinkList");let v=document.querySelector("#teaTypeSelect"),y=document.querySelector("#ingredientsSelect"),L=[],p=[],k=[];const b=sessionStorage.getItem("user_id"),C=()=>{const a=p.map(t=>!t||!t.TeaType?"":t.Ingredients.length===0?`${t.TeaType}`:`${t.TeaType},${t.Ingredients}`),e=[];for(let t=0;t<a.length;t++)e.push(a[t].split(","));let n="",s=[];e.forEach(t=>{t.forEach((m,d)=>{n+=`<li class="drinks-tag">${m}</li>`,d===t.length-1&&(s.push(n),n="")}),k.push(s),s=[]})},_=()=>{let a="";p.forEach(e=>{a+=`
           <li class="drinks-card px-16 py-24 px-md-24" data-drink-id="${e.id}">
             <button type="button" class="collect-btn border-0 text-primary fa-regular fa-heart fs-24"
               value="collected"></button>
@@ -22,7 +22,7 @@ import"./bootstrap.min-66c8f441.js";const p=document.querySelector("#drinkList")
                   </span>搜尋店家</a>
               </div>
           </li>
-        `}),p.innerHTML=a},N=()=>{_(),E(u)},h=()=>{const a=e=>document.querySelector(`.drinks-card[data-drink-id="${e}"] .collect-btn`);L.forEach(e=>{const n=a(e.drinkId);n&&(n.value="collected",n.classList.remove("fa-regular"),n.classList.add("fa-solid"))})};p.addEventListener("click",function(a){const e=a.target.closest(".collect-btn");if(e){if(!b)console.log("無權限: 沒有找到 Token"),M();else if(a.target.classList.contains("collect-btn")){const s=e.closest(".drinks-card").dataset.drinkId;console.log(s);const t="https://json-server-project-wtkt.onrender.com/userDrinkCollections",m=`${t}?userId=${b}&drinkId=${s}`;axios.get(m).then(o=>{if(o.data.length>0){console.log("已收藏改未收藏"),e.value="uncollect",e.classList.remove("fa-solid"),e.classList.add("fa-regular"),axios.delete(`${t}/${o.data[0].id}`).then(r=>{console.log("收藏已刪除",r),alert("已取消收藏")}).catch(r=>{console.error("Error deleting collection:",r)});return}else console.log("未收藏改已收藏"),e.value="collected",e.classList.remove("fa-regular"),e.classList.add("fa-solid");axios.post(t,{userId:parseInt(b),drinkId:parseInt(s)}).then(r=>{console.log("收藏已新增",r),alert("已新增收藏")}).catch(r=>{console.error("Error adding collection:",r)})}).catch(o=>{console.error("Error checking collection status:",o)})}}});function M(){alert("登入後即可使用收藏功能"),window.location.href="logIn.html"}v.addEventListener("change",x);y.addEventListener("change",x);function x(){let a=u;v.value!==""&&(a=a.filter(e=>e.TeaType.includes(v.value))),y.value!==""&&(a=a.filter(e=>e.Ingredients.includes(y.value))),w(a),E(a),$(a),h()}function w(a){let e="";a.forEach(n=>{e+=`
+        `}),u.innerHTML=a},N=()=>{_(),E(p)},h=()=>{const a=e=>document.querySelector(`.drinks-card[data-drink-id="${e}"] .collect-btn`);L.forEach(e=>{const n=a(e.drinkId);n&&(n.value="collected",n.classList.remove("fa-regular"),n.classList.add("fa-solid"))})};u.addEventListener("click",function(a){const e=a.target.closest(".collect-btn");if(e){if(!b)console.log("無權限: 沒有找到 Token"),M();else if(a.target.classList.contains("collect-btn")){const s=e.closest(".drinks-card").dataset.drinkId;console.log(s);const t="https://json-server-project-wtkt.onrender.com/userDrinkCollections",m=`${t}?userId=${b}&drinkId=${s}`;axios.get(m).then(d=>{if(d.data.length>0){e.value="uncollect",e.classList.remove("fa-solid"),e.classList.add("fa-regular"),axios.delete(`${t}/${d.data[0].id}`).then(r=>{Swal.fire("已取消收藏")}).catch(r=>{console.error("Error deleting collection:",r)});return}else e.value="collected",e.classList.remove("fa-regular"),e.classList.add("fa-solid");axios.post(t,{userId:parseInt(b),drinkId:parseInt(s)}).then(r=>{Swal.fire("收藏成功")}).catch(r=>{console.error("Error adding collection:",r)})}).catch(d=>{console.error("Error checking collection status:",d)})}}});function M(){alert("登入後即可使用收藏功能"),window.location.href="logIn.html"}v.addEventListener("change",x);y.addEventListener("change",x);function x(){let a=p;v.value!==""&&(a=a.filter(e=>e.TeaType.includes(v.value))),y.value!==""&&(a=a.filter(e=>e.Ingredients.includes(y.value))),w(a),E(a),$(a),h()}function w(a){let e="";a.forEach(n=>{e+=`
       <li class="drinks-card px-16 py-24 px-md-24" data-drink-id="${n.id}">
         <button type="button" class="collect-btn border-0 text-primary fa-regular fa-heart fs-24"
           value="collected"></button>
@@ -46,7 +46,7 @@ import"./bootstrap.min-66c8f441.js";const p=document.querySelector("#drinkList")
                   </span>搜尋店家</a>
           </div>
       </li>
-    `}),p.innerHTML=e}const $=a=>{const e=a.length,n=10,s=Math.ceil(e/n);let t=1;function m(c){const d=(c-1)*n,i=d+n,f=a.slice(d,i);o(f),I(),h()}function o(c){p.innerHTML="";let d="";c.forEach(i=>{d+=`
+    `}),u.innerHTML=e}const $=a=>{const e=a.length,n=10,s=Math.ceil(e/n);let t=1;function m(c){const o=(c-1)*n,i=o+n,f=a.slice(o,i);d(f),I(),h()}function d(c){u.innerHTML="";let o="";c.forEach(i=>{o+=`
           <li class="drinks-card px-16 py-24 px-md-24" data-drink-id="${i.id}">
             <button type="button" class="collect-btn border-0 text-primary fa-regular fa-heart fs-24"
               value="collected"></button>
@@ -70,7 +70,7 @@ import"./bootstrap.min-66c8f441.js";const p=document.querySelector("#drinkList")
                   </span>搜尋店家</a>
               </div>
           </li>
-        `}),p.innerHTML=d}function I(){const c=document.getElementById("pagination");c.innerHTML="";let d="";for(let l=1;l<=s;l++)d+=`
+        `}),u.innerHTML=o}function I(){const c=document.getElementById("pagination");c.innerHTML="";let o="";for(let l=1;l<=s;l++)o+=`
       <li class="page-item mx-4 ${l===t?"active":""}">   
         <a class="page-link" href="#" data-page="${l}">${l}</a>
       </li>
@@ -87,7 +87,7 @@ import"./bootstrap.min-66c8f441.js";const p=document.querySelector("#drinkList")
       </a>
     </li>
 
-    ${d}
+    ${o}
 
     <li id="next-page-btn" class="page-item mx-4 d-none d-md-block ${S}">
       <a class="page-link ${f}" href="#" aria-label="Next">
@@ -99,4 +99,4 @@ import"./bootstrap.min-66c8f441.js";const p=document.querySelector("#drinkList")
       <a class="page-link ${f}" href="#" aria-label="Next">
         <span class="material-symbols-outlined align-middle">keyboard_double_arrow_right</span>
       </a>
-    </li>`,document.querySelectorAll(".page-link").forEach(l=>{l.addEventListener("click",D=>{D.preventDefault();const j=parseInt(l.dataset.page);r(j)})});const g=[t];document.getElementById("previousPageBtn").addEventListener("click",l=>{l.preventDefault(),g[0]>1&&r(g[0]-1)}),document.getElementById("next-page-btn").addEventListener("click",l=>{l.preventDefault(),g[0]!==s&&r(g[0]+1)}),document.getElementById("first-page-btn").addEventListener("click",l=>{l.preventDefault(),g[0]>1&&r(1)}),document.getElementById("last-page-btn").addEventListener("click",l=>{l.preventDefault(),g[0]!==s&&r(s)})}function r(c){t=c,m(t),window.scrollTo(0,400)}m(t)},E=a=>{const e=document.querySelector("#searchDrinks"),n=()=>{let s=a.filter(t=>t.DrinkName.includes(e.value));w(s),$(s),h()};e.addEventListener("keyup",s=>{s.keyCode===13&&n()})};Promise.all([axios.get(`https://json-server-project-wtkt.onrender.com/userDrinkCollections?userId=${b}`),axios.get("https://json-server-project-wtkt.onrender.com/drinks")]).then(a=>{L=a[0].data,u=a[1].data,C(),N(),x(),$(u),h()}).catch(a=>{console.error("Error fetching data:",a)});
+    </li>`,document.querySelectorAll(".page-link").forEach(l=>{l.addEventListener("click",D=>{D.preventDefault();const j=parseInt(l.dataset.page);r(j)})});const g=[t];document.getElementById("previousPageBtn").addEventListener("click",l=>{l.preventDefault(),g[0]>1&&r(g[0]-1)}),document.getElementById("next-page-btn").addEventListener("click",l=>{l.preventDefault(),g[0]!==s&&r(g[0]+1)}),document.getElementById("first-page-btn").addEventListener("click",l=>{l.preventDefault(),g[0]>1&&r(1)}),document.getElementById("last-page-btn").addEventListener("click",l=>{l.preventDefault(),g[0]!==s&&r(s)})}function r(c){t=c,m(t),window.scrollTo(0,400)}m(t)},E=a=>{const e=document.querySelector("#searchDrinks"),n=()=>{let s=a.filter(t=>t.DrinkName.includes(e.value));w(s),$(s),h()};e.addEventListener("keyup",s=>{s.keyCode===13&&n()})};Promise.all([axios.get(`https://json-server-project-wtkt.onrender.com/userDrinkCollections?userId=${b}`),axios.get("https://json-server-project-wtkt.onrender.com/drinks")]).then(a=>{L=a[0].data,p=a[1].data,C(),N(),x(),$(p),h()}).catch(a=>{console.error("Error fetching data:",a)});
---
+++ assets/stores-list-58e7d699.js
@@ -0,0 +1,102 @@
+import"./bootstrap.min-66c8f441.js";const g=document.querySelector("#storeList");let m=document.querySelector("#deliverySelect"),L=document.querySelector("#CooperationSelect"),P=[],p=[],v=[];const h=sessionStorage.getItem("user_id"),j=()=>{p.forEach(e=>{const t={uber:"uber eat",foodpanda:"food panda",hasEvent:"合作活動"};let s="";for(const a in t)e[a]&&(s+=`<li class="stores-tag">${t[a]}</li>`);v.push(s),s=""})},_=()=>{let e="";p.forEach(t=>{e+=`
+        <li class="stores-card" data-shop-id="${t.id}">
+            <button type="button" class="collect-btn border-0 text-primary fa-regular fa-heart fs-24"
+              value="collected"></button>
+            <img src="${t.logo}" class="mb-8" alt="store image">
+            <div class="stores-card-body px-16 pt-8 pb-24 px-md-24">
+              <div class="d-flex justify-content-between">
+                <h5 class="mb-8 mb-md-12">${t.name}</h5>
+                <ul class="d-flex text-primary">
+                  <li><i class="fa-solid fa-star"></i></li>
+                  <li><i class="fa-solid fa-star"></i></li>
+                  <li><i class="fa-solid fa-star"></i></li>
+                  <li><i class="fa-solid fa-star"></i></li>
+                  <li><i class="fa-regular fa-star"></i></li>
+                </ul>
+              </div>
+              <p class="stores-card-content mb-16">
+                ${t.Description}</p>
+              <ul class="stores-tag-group mb-16">
+                ${v[t.id-1]}
+              </ul>
+              <a href="stores-info.html?id=${t.id}" class="stores-card-btn">查看店家資訊</a>
+            </div>
+          </li>
+        `}),g.innerHTML=e},M=()=>{_(),S(p)},y=()=>{const e=t=>document.querySelector(`.stores-card[data-shop-id="${t}"] .collect-btn`);P.forEach(t=>{const s=e(t.shopId);s&&(s.value="collected",s.classList.remove("fa-regular"),s.classList.add("fa-solid"))})};g.addEventListener("click",function(e){if(e.target.closest(".collect-btn")){if(!h)console.log("無權限: 沒有找到 Token"),q();else if(e.target.classList.contains("collect-btn")){const s=e.target,l=s.closest(".stores-card").dataset.shopId;console.log(l);const c="https://json-server-project-wtkt.onrender.com/userShopCollections",x=`${c}?userId=${h}&shopId=${l}`;axios.get(x).then(d=>{if(d.data.length>0){s.value="uncollect",s.classList.remove("fa-solid"),s.classList.add("fa-regular"),axios.delete(`${c}/${d.data[0].id}`).then(r=>{Swal.fire("已取消收藏")}).catch(r=>{console.error("Error deleting collection:",r)});return}else s.value="collected",s.classList.remove("fa-regular"),s.classList.add("fa-solid");axios.post(c,{userId:parseInt(h),shopId:parseInt(l)}).then(r=>{Swal.fire("收藏成功")}).catch(r=>{console.error("Error adding collection:",r)})}).catch(d=>{console.error("Error checking collection status:",d)})}}});function q(){alert("登入後即可使用收藏功能"),window.location.href="logIn.html"}m.addEventListener("change",$);L.addEventListener("change",$);function $(){let e=p;m.value!==""&&(m.value==="uber eat"?e=e.filter(t=>t.uber):m.value==="food panda"&&(e=e.filter(t=>t.foodpanda))),L.value!==""&&(e=e.filter(t=>t.hasEvent)),I(e),S(e),k(e),y()}function I(e){let t="";e.forEach(s=>{t+=`
+      <li class="stores-card" data-shop-id="${s.id}">
+      <button type="button" class="collect-btn border-0 text-primary fa-regular fa-heart fs-24"
+        value="collected"></button>
+      <img src="${s.logo}" class="mb-8" alt="store image">
+      <div class="stores-card-body px-16 pt-8 pb-24 px-md-24">
+        <div class="d-flex justify-content-between">
+          <h5 class="mb-8 mb-md-12">${s.name}</h5>
+          <ul class="d-flex text-primary">
+            <li><i class="fa-solid fa-star"></i></li>
+            <li><i class="fa-solid fa-star"></i></li>
+            <li><i class="fa-solid fa-star"></i></li>
+            <li><i class="fa-solid fa-star"></i></li>
+            <li><i class="fa-regular fa-star"></i></li>
+          </ul>
+        </div>
+        <p class="stores-card-content mb-16">
+          ${s.Description}</p>
+        <ul class="stores-tag-group mb-16">
+          ${v[s.id-1]}
+        </ul>
+        <a href="stores-info.html" class="stores-card-btn">查看店家資訊</a>
+      </div>
+    </li>
+    `}),g.innerHTML=t}const k=e=>{const t=e.length,s=12,a=Math.ceil(t/s);let l=1;function c(r){const n=(r-1)*s,i=n+s,b=e.slice(n,i);x(b),d(),y()}function x(r){g.innerHTML="";let n="";r.forEach(i=>{n+=`
+          <li class="stores-card" data-shop-id="${i.id}">
+          <button type="button" class="collect-btn border-0 text-primary fa-regular fa-heart fs-24"
+            value="collected"></button>
+          <img src="${i.logo}" class="mb-8" alt="store image">
+          <div class="stores-card-body px-16 pt-8 pb-24 px-md-24">
+            <div class="d-flex justify-content-between">
+              <h5 class="mb-8 mb-md-12">${i.name}</h5>
+              <ul class="d-flex text-primary">
+                <li><i class="fa-solid fa-star"></i></li>
+                <li><i class="fa-solid fa-star"></i></li>
+                <li><i class="fa-solid fa-star"></i></li>
+                <li><i class="fa-solid fa-star"></i></li>
+                <li><i class="fa-regular fa-star"></i></li>
+              </ul>
+            </div>
+            <p class="stores-card-content mb-16">
+              ${i.Description}</p>
+            <ul class="stores-tag-group mb-16">
+              ${v[i.id-1]}
+            </ul>
+            <a href="stores-info.html?id=${i.id}" class="stores-card-btn">查看店家資訊</a>
+          </div>
+        </li>
+          `}),g.innerHTML=n}function d(){const r=document.getElementById("pagination");r.innerHTML="";let n="";for(let o=1;o<=a;o++)n+=`
+        <li class="page-item mx-4 ${o===l?"active":""}">   
+          <a class="page-link" href="#" data-page="${o}">${o}</a>
+        </li>
+      `;const i=l===1?"text-gray":"",b=l===a?"text-gray":"",D=l===1?"disabled":"",B=l===1?"disabled":"",w=l===a?"disabled":"",C=l===a?"disabled":"";r.innerHTML=`
+      <li id="first-page-btn" class="page-item mx-4 d-none d-md-block ${D}">
+        <a class="page-link ${i}" href="#" aria-label="Previous">
+          <span class="material-symbols-outlined align-middle">keyboard_double_arrow_left</span>
+        </a>
+      </li>
+  
+      <li id="previousPageBtn" class="page-item mx-4 d-none d-md-block ${B}">
+        <a class="page-link ${i}" href="#" aria-label="Previous">
+          <span class="material-symbols-outlined align-middle">chevron_left</span>
+        </a>
+      </li>
+  
+      ${n}
+  
+      <li id="next-page-btn" class="page-item mx-4 d-none d-md-block ${w}">
+        <a class="page-link ${b}" href="#" aria-label="Next">
+          <span class="material-symbols-outlined align-middle">chevron_right</span>
+        </a>
+      </li>
+  
+      <li id="last-page-btn" class="page-item mx-4 d-none d-md-block ${C}">
+        <a class="page-link ${b}" href="#" aria-label="Next">
+          <span class="material-symbols-outlined align-middle">keyboard_double_arrow_right</span>
+        </a>
+      </li>`,document.querySelectorAll(".page-link").forEach(o=>{o.addEventListener("click",E=>{E.preventDefault();const T=parseInt(o.dataset.page);f(T)})});const u=[l];document.getElementById("previousPageBtn").addEventListener("click",o=>{o.preventDefault(),u[0]>1&&f(u[0]-1)}),document.getElementById("next-page-btn").addEventListener("click",o=>{o.preventDefault(),u[0]!==a&&f(u[0]+1)}),document.getElementById("first-page-btn").addEventListener("click",o=>{o.preventDefault(),u[0]>1&&f(1)}),document.getElementById("last-page-btn").addEventListener("click",o=>{o.preventDefault(),u[0]!==a&&f(a)})}function f(r){l=r,c(l),window.scrollTo(0,400)}c(l)},S=e=>{const t=document.querySelector("#searchStores"),s=()=>{let a=e;t.value!==""&&(a=a.filter(l=>l.name.includes(t.value))),I(a),k(a),y()};t.addEventListener("keydown",a=>{a.keyCode===13&&s()})};Promise.all([axios.get(`https://json-server-project-wtkt.onrender.com/userShopCollections?userId=${h}`),axios.get("https://json-server-project-wtkt.onrender.com/shops")]).then(e=>{P=e[0].data,p=e[1].data,j(),M(),$(),k(p),y()}).catch(e=>{console.error("Error fetching data:",e)});
