45b4f668bb32365f962f6c46f95824ddd6e1ad8e
Yu-Chuan, Ciou <z111048@gmail.com>
2023-12-05 07:09:26 +0800

    登入、註冊程式碼優化

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,130 +1,237 @@
-// function getCookie(name) {
-//   var nameEQ = name + "=";
-//   var ca = document.cookie.split(";");
-//   for (var i = 0; i < ca.length; i++) {
-//     var c = ca[i];
-//     while (c.charAt(0) == " ") c = c.substring(1);
-//     if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+// const urlParams = new URLSearchParams(window.location.search);
+// const JWT_token = urlParams.get("token");
+// console.log(JWT_token);
+
+// async function validateUser(token) {
+//   try {
+//     const res = await axios.get("https://authbridge.onrender.com/user", {
+//       headers: {
+//         Authorization: token,
+//       },
+//     });
+
+//     if (res && res.data.email) {
+//       console.log("用戶身份驗證成功:", res.data.email);
+//       loginUser(res.data.email, token);
+//     } else {
+//       console.log("用戶身份驗證失敗");
+//     }
+//   } catch (err) {
+//     console.error("身份驗證過程中發生錯誤:", err);
 //   }
-//   return null;
 // }
 
-const urlParams = new URLSearchParams(window.location.search);
-const JWT_token = urlParams.get("token");
-console.log(JWT_token);
-if (JWT_token) {
-  console.log("找到 token:", JWT_token);
-  axios
-    .get("https://authbridge.onrender.com/user", {
-      headers: {
-        Authorization: JWT_token,
-      },
-    })
-    .then((res) => {
-      if (res) {
-        const email = res.data.email;
-        loginUser(email, JWT_token);
-        // console.log(res);
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-} else {
-  console.log("未找到指定的 token");
-}
+// if (JWT_token) {
+//   console.log("找到 token:", JWT_token);
+//   validateUser(JWT_token);
+// } else {
+//   console.log("未找到指定的 token");
+// }
 
-const token = sessionStorage.getItem("jwtToken");
+// const token = sessionStorage.getItem("jwtToken");
+// function isAuthenticated() {
+//   if (token) {
+//     console.log("有權限: 找到 Token");
+//     return true;
+//   }
+//   console.log("無權限: 未找到 Token");
+//   return false;
+// }
+// isAuthenticated();
 
-// 確認是否登入
-function isAuthenticated() {
-  if (token) {
-    console.log("有權限: 找到 Token");
-    // redirectToMember();
-    return true;
-  }
-  return false;
-}
-isAuthenticated();
+// document
+//   .getElementById("loginForm")
+//   .addEventListener("submit", function (event) {
+//     event.preventDefault(); // 阻止表單的預設提交行為
 
-// document.getElementById("loginForm").addEventListener("submit", loginUser);
+//     const email = document.getElementById("logInEmail").value;
+//     const password = document.getElementById("logInPassword").value;
 
-// async function loginUser(event) {
-//   event.preventDefault(); // 阻止表單的預設提交行為
+//     loginUser(email, password); // 使用從表單獲取的帳號和密碼調用 loginUser
+//   });
 
-//   const email = document.getElementById("logInEmail").value;
-//   const password = document.getElementById("logInPassword").value;
-//   // // http://localhost:3000/login
+// async function loginUser(email, password) {
 //   try {
 //     const res = await axios.post(
 //       "https://drinkpicker-nclv.onrender.com/login",
 //       { email, password }
 //     );
 
-//     console.log(res.data);
-//     const accessToken = res.data.accessToken;
-//     const user_email = res.data.user.email;
-//     const user_id = res.data.user.id;
-//     const user_nickname = res.data.user.nickname;
-//     if (accessToken) {
+//     if (res.status === 200 && res.data.accessToken) {
 //       console.log("登入成功", res.data);
-//       sessionStorage.setItem("jwtToken", accessToken);
-//       sessionStorage.setItem("user_email", user_email);
-//       sessionStorage.setItem("user_id", user_id);
-//       sessionStorage.setItem("user_nickname", user_nickname);
+//       handleSessionStorage(res.data); // 處理 sessionStorage 的操作
+
 //       redirectToHome(); // 登入成功後的操作，例如跳轉到另一個頁面
 //     } else {
-//       alert("登入失敗，請檢查帳號或密碼"); // 登入失敗後的操作
+//       handleLoginError(); // 登入失敗的處理
 //     }
 //   } catch (error) {
 //     console.error("登入錯誤", error);
-//     alert("登入失敗，請檢查帳號或密碼"); // 登入失敗後的操作
+//     handleLoginError(); // 登入失敗的處理
 //   }
 // }
 
-document
-  .getElementById("loginForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault(); // 阻止表單的預設提交行為
+// function handleSessionStorage(data) {
+//   sessionStorage.setItem("jwtToken", data.accessToken);
+//   sessionStorage.setItem("user_email", data.user.email);
+//   sessionStorage.setItem("user_id", data.user.id);
+//   sessionStorage.setItem("user_nickname", data.user.nickname);
+//   sessionStorage.setItem("user_picture", data.user.picture);
+// }
 
-    const email = document.getElementById("logInEmail").value;
-    const password = document.getElementById("logInPassword").value;
+// function handleLoginError() {
+//   alert("登入失敗，請檢查帳號或密碼"); // 登入失敗後的操作
+// }
 
-    loginUser(email, password); // 使用從表單獲取的帳號和密碼調用 loginUser
-  });
+// function redirectToHome() {
+//   window.location.href = "index.html";
+// }
 
-async function loginUser(email, password) {
+// 環境變量
+const AUTH_API = "https://authbridge.onrender.com/user";
+const LOGIN_API = "https://drinkpicker-nclv.onrender.com/login";
+const HOME_URL = "index.html";
+
+// 獲取 JWT 令牌
+const JWT_token = new URLSearchParams(window.location.search).get("token");
+
+// 驗證用戶
+async function validateUser(token) {
   try {
-    const res = await axios.post(
-      "https://drinkpicker-nclv.onrender.com/login",
-      { email, password }
-    );
-
-    console.log(res.data);
-    const accessToken = res.data.accessToken;
-    const user_email = res.data.user.email;
-    const user_id = res.data.user.id;
-    const user_nickname = res.data.user.nickname;
-    const user_picture = res.data.user.picture;
-
-    if (accessToken) {
-      console.log("登入成功", res.data);
-      sessionStorage.setItem("jwtToken", accessToken);
-      sessionStorage.setItem("user_email", user_email);
-      sessionStorage.setItem("user_id", user_id);
-      sessionStorage.setItem("user_nickname", user_nickname);
-      sessionStorage.setItem("user_picture", user_picture);
-
-      redirectToHome(); // 登入成功後的操作，例如跳轉到另一個頁面
+    const res = await sendRequest(AUTH_API, "GET", token);
+    if (res && res.data.email) {
+      console.log("用戶身份驗證成功:", res.data.email);
+      loginUser(res.data.email, token);
     } else {
-      alert("登入失敗，請檢查帳號或密碼"); // 登入失敗後的操作
+      console.log("用戶身份驗證失敗");
     }
-  } catch (error) {
-    console.error("登入錯誤", error);
-    alert("登入失敗，請檢查帳號或密碼"); // 登入失敗後的操作
+  } catch (err) {
+    console.error("身份驗證過程中發生錯誤:", err);
   }
 }
 
-function redirectToHome() {
-  window.location.href = "index.html";
+// 登入用戶
+async function loginUser(email, password) {
+  // 測試函數
+  const isValid = true;
+  // const { isValid, errors } = validateForm(email, password);
+  console.log(isValid); // 這將會顯示 true 或 false
+  // console.log(errors); // 這將會顯示一個包含所有錯誤訊息的陣列
+
+  if (!isValid) return;
+
+  try {
+    const res = await sendRequest(LOGIN_API, "POST", null, { email, password });
+    if (res.status === 200 && res.data.accessToken) {
+      console.log("登入成功", res.data);
+      handleSessionStorage(res.data);
+      redirectToHome();
+    } else {
+      handleLoginError();
+    }
+  } catch (error) {
+    console.error("登入錯誤", error);
+    handleLoginError();
+  }
 }
+
+function validateForm(email, password) {
+  let errors = [];
+
+  // 郵件地址驗證
+  if (!validator.isEmail(email)) {
+    errors.push("無效的郵件地址");
+  }
+
+  // 密碼長度至少為 8，並且需要包含至少一個數字、一個大寫字母、一個小寫字母和一個特殊字符
+  if (
+    !validator.isLength(password, { min: 8 }) ||
+    !/\d/.test(password) ||
+    !/[A-Z]/.test(password) ||
+    !/[a-z]/.test(password) ||
+    !/[!@#$%^&*(),.?":{}|<>]/.test(password)
+  ) {
+    errors.push(
+      "密碼長度至少為 8，並且需要包含至少一個數字、一個大寫字母、一個小寫字母和一個特殊字符"
+    );
+  }
+
+  if (errors.length > 0) {
+    Swal.fire({
+      title: "驗證錯誤",
+      icon: "error",
+      html: errors.join("<br>"),
+    });
+  } else {
+    Swal.fire({
+      title: "成功",
+      icon: "success",
+      text: "表單驗證成功！",
+    });
+  }
+
+  return {
+    isValid: errors.length === 0,
+    errors,
+  };
+}
+
+// 處理 Session Storage
+function handleSessionStorage(data) {
+  sessionStorage.setItem("jwtToken", data.accessToken);
+  sessionStorage.setItem("user_email", data.user.email);
+  sessionStorage.setItem("user_id", data.user.id);
+  sessionStorage.setItem("user_nickname", data.user.nickname);
+  sessionStorage.setItem("user_picture", data.user.picture);
+}
+
+// 登入錯誤處理
+function handleLoginError() {
+  alert("登入失敗，請檢查帳號或密碼");
+}
+
+// 重定向到首頁
+function redirectToHome() {
+  window.location.href = HOME_URL;
+}
+
+// AJAX 請求函數
+async function sendRequest(url, method, token = "", data = {}) {
+  const config = {
+    method: method,
+    url: url,
+    headers: { Authorization: token },
+    data: data,
+  };
+  return await axios(config);
+}
+
+// 檢查是否已登入
+function isAuthenticated() {
+  const token = sessionStorage.getItem("jwtToken");
+  if (token) {
+    console.log("找到 Session Token");
+    return true;
+  }
+  console.log("未找到 Session Token");
+  return false;
+}
+
+// 事件監聽器
+document
+  .getElementById("loginForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    const email = document.getElementById("logInEmail").value;
+    const password = document.getElementById("logInPassword").value;
+    loginUser(email, password);
+  });
+
+if (JWT_token) {
+  console.log("找到 JWT_token", JWT_token);
+  validateUser(JWT_token);
+} else {
+  console.log("未找到 JWT_token");
+}
+
+isAuthenticated();
--- assets/js/signUp.js
+++ assets/js/signUp.js
@@ -1,6 +1,48 @@
-document.getElementById("signUpForm").addEventListener("submit", loginUser);
+// document.getElementById("signUpForm").addEventListener("submit", loginUser);
 
-async function loginUser(event) {
+// async function loginUser(event) {
+//   event.preventDefault();
+
+//   const nickname = document.getElementById("signUpName").value;
+//   const email = document.getElementById("signUpEmail").value;
+//   const password = document.getElementById("signUpPassword").value;
+//   const confirmPassword = document.getElementById("signUpPassword2").value;
+
+//   if (password !== confirmPassword) {
+//     alert("密碼和確認密碼不匹配！");
+//     return;
+//   }
+
+//   // http://localhost:3000/register
+//   // https://drinkpicker-nclv.onrender.com/register
+//   try {
+//     const res = await axios.post(
+//       "https://drinkpicker-nclv.onrender.com/register",
+//       {
+//         nickname,
+//         email,
+//         password,
+//       }
+//     );
+//     redirectToHome();
+//     console.log("註冊成功", res.data);
+//     // 註冊成功後的操作，例如跳轉到登入頁面
+//   } catch (error) {
+//     console.error("註冊失敗", error);
+//     alert(error.response.data);
+//     // 處理錯誤，例如顯示錯誤訊息
+//   }
+// }
+
+// function redirectToHome() {
+//   window.location.href = "index.html";
+// }
+
+document
+  .getElementById("signUpForm")
+  .addEventListener("submit", handleSignUpSubmit);
+
+async function handleSignUpSubmit(event) {
   event.preventDefault();
 
   const nickname = document.getElementById("signUpName").value;
@@ -8,13 +50,10 @@ async function loginUser(event) {
   const password = document.getElementById("signUpPassword").value;
   const confirmPassword = document.getElementById("signUpPassword2").value;
 
-  if (password !== confirmPassword) {
-    alert("密碼和確認密碼不匹配！");
-    return;
+  if (!isFormValid(nickname, email, password, confirmPassword)) {
+    return; // 表單驗證未通過
   }
 
-  // http://localhost:3000/register
-  // https://drinkpicker-nclv.onrender.com/register
   try {
     const res = await axios.post(
       "https://drinkpicker-nclv.onrender.com/register",
@@ -24,14 +63,46 @@ async function loginUser(event) {
         password,
       }
     );
-    redirectToHome();
     console.log("註冊成功", res.data);
-    // 註冊成功後的操作，例如跳轉到登入頁面
+    Swal.fire("註冊成功！", "", "success").then(() => redirectToHome()); // 使用 SweetAlert2 顯示成功信息
   } catch (error) {
     console.error("註冊失敗", error);
-    alert(error.response.data);
-    // 處理錯誤，例如顯示錯誤訊息
+    Swal.fire("註冊失敗", error.response.data, "error"); // 使用 SweetAlert2 顯示錯誤信息
   }
+}
+
+function isFormValid(nickname, email, password, confirmPassword) {
+  // 郵件地址格式驗證
+  if (!validator.isEmail(email)) {
+    Swal.fire("錯誤", "無效的郵件地址", "error");
+    return false;
+  }
+
+  // 密碼與確認密碼一致性檢查
+  if (password !== confirmPassword) {
+    Swal.fire("錯誤", "密碼和確認密碼不匹配！", "error");
+    return false;
+  }
+
+  // 密碼安全性驗證
+  if (
+    !validator.isStrongPassword(password, {
+      minLength: 8,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+    })
+  ) {
+    Swal.fire(
+      "錯誤",
+      "密碼安全性不足。請確保密碼至少包含 8 個字符，並且包括大寫字母、小寫字母、數字及特殊符號。",
+      "error"
+    );
+    return false;
+  }
+
+  return true;
 }
 
 function redirectToHome() {
